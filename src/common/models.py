b3cbb5a55355457781999eaf728b17d604f89637
Alfred Santacatalina <alfred82santa@gmail.com>
2013-10-11 20:12:04 +0200

    Init app & orgs

---
+++ src/common/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+class CommonModel(models.Model):
+    creation_date = models.DateTimeField('creation datetime', auto_now_add=True)
+    modified_date = models.DateTimeField('modified datetime', auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True,
+                                   null=True, on_delete=models.SET_NULL,
+                                   related_name="created_%(app_label)s_%(class)s",
+                                   related_query_name="creations"
+                                   )
+    modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True,
+                                   null=True, on_delete=models.SET_NULL,
+                                   related_name="modification_%(app_label)s_%(class)s",
+                                   related_query_name="modifications"
+                                   )
+    class Meta:
+        abstract = True
